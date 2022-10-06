1274be1d0cf4360110cd973d67ced6483510e881
sp <santo221b@gmail.com>
2022-10-06 20:49:32 +0530

    my first commit

---
+++ detect_duplicate.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+int main()
+{
+    char all[5][10] = {"Stark", "Logan", "Stark", "Logan", "Logan"};
+    char temp[10] = "zero";
+    int i = 0, j;
+    char unique[5][10] = {};
+    // memcpy(all[0], dark, sizeof(dark));
+    for (i = 0; i < 5; i++)
+    {
+        for (j = i + 1; j < 5; j++)
+        {
+            if (strcmp(all[i], all[j]) == 0)
+            {
+                memcpy(all[j], temp, sizeof(temp));
+            }
+        }
+    }
+    for (i = 0; i < 5; i++)
+    {
+        if (strcmp(all[i], temp) != 0)
+        {
+            memcpy(unique[i], all[i], sizeof(all[i]));
+        }
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("%s\t", unique[i]);
+    }
+    return 0;
+}
